faebdab399d1ad454ce6df6b8d2db4f5f1c85a77
Gobinda Paul <33730099+gobinda1547@users.noreply.github.com>
2018-04-24 12:56:25 +0600

    added basic syntax

---
+++ Basic Syntax/Basic Syntax/main.m
@@ -0,0 +1,20 @@
+//
+//  main.m
+//  Basic Syntax
+//
+//  Created by Gobinda Paul on 4/23/18.
+//  Copyright © 2018 Gobinda Paul. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+        int x = 5;
+        NSLog(@"the value of x is %d\n",x);
+    }
+    return 0;
+}
